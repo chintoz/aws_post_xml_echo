1b5fb95049b09efadd7894df73826f376a0e87c5
jjmena80 <chintoz_ltd@hotmail.com>
2018-02-16 23:53:06 +0100

    Initial server application

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+//var path = require('path');
+//var routes = require('./routes/index');
+
+var app = express();
+
+// Configure routing
+//app.use('/', routes);
+//app.use(bodyParser.json());
+
+
+// export app as a global variable
+module.exports = app;
