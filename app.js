91c654ccb4ed2c20412db45674fcdedffc6ba194
jjmena80 <chintoz_ltd@hotmail.com>
2018-02-21 23:06:27 +0100

    Changes for raw body middleware

--- app.js
+++ app.js
@@ -5,11 +5,21 @@ var routes = require('./routes/routes');
 
 var app = express();
 
+// Function to read raw body
+function rawBody(req, res, next) {
+    req.setEncoding('utf8');
+    req.rawBody = '';
+    req.on('data', function(chunk) {
+        req.rawBody += chunk;
+    });
+    req.on('end', function(){
+        next();
+    });
+}
+  
 // Configure routing
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.xml());
-app.use('/xml', routes);
-
+app.use(rawBody);
+app.use('/', routes);
 
 // export app as a global variable
 module.exports = app;
